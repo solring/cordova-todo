9e74b4d2c7517dfecc66759dde28d450eb2f9a96
Solring Lin <sherry12714@gmail.com>
2021-11-10 20:14:01 +0000

    add checkbox and use local storage.

--- www/js/index.js
+++ www/js/index.js
@@ -29,39 +29,104 @@ function onDeviceReady() {
 }
 
 // ========= below is main ==========
+const LS_KEY_COUNTER = "todo_counter"
+const LS_KEY_TODOLIST = "todo_list"
 
-let counter = 0;
+const store = window.localStorage
+let counter = parseInt(store.getItem(LS_KEY_COUNTER),10) || 0
+let json = store.getItem(LS_KEY_TODOLIST)
+let todos = json ? JSON.parse(json) : {}
+
+function save() {
+    console.log("save:")
+    store.setItem(LS_KEY_TODOLIST, JSON.stringify(todos))
+}
+
+// Handlers
+function handleChecked(e) {
+    let checked = e.target.checked
+    let item = $(e.target.parentElement)
+    if(!item) {
+        console.warn("handleChecked: item not found.");
+        return
+    }
+    checked ? item.addClass('completed') : item.removeClass('completed')
+    item.find('input[type="text"]').attr('disabled', checked)
+}
+
+function handleBlur(e) {
+    let text = e.target.value
+    let item = e.target.parentElement
+    if(!item) {
+        console.warn("handleBlur: todo item not found.")
+    }
+
+    let key = $(item).attr('key')
+    todos[key] = text
+    save()
+}
 
 function remove(key) {
     $('#todo-list').find(`li[key="${key}"]`).remove()
+    delete todos[key]
+    save()
 }
 
-function add() {
-    let key = `${counter}`
-    console.log("add:")
+function newNode(key=null, value="" ) {
+    console.log("newNode:");
+    if(!key)
+        key = `key ${counter}`
 
     $('#todo-list').append(
         $('<li></li>')
-            .addClass('list-group-item list-group-item-action d-flex justify-content-between')
+            .addClass('list-group-item d-flex')
             .attr('key', key)
+            .append(
+                $('<input type="checkbox" />')
+                    .addClass("form-check-input")
+                    .addClass("flex-shrink-0")
+                    .on('change', handleChecked)
+            )
             .append(
                 $('<input type="text">')
                     .attr('placeholder', `New Item ${counter}`)
+                    .attr('value', value)
+                    .addClass('form-control ms-2')
+                    .on('blur', handleBlur)
             )
             .append(
                 $('<button>-</button>')
-                    .addClass('btn btn-primary bg-theme border-0')
+                    .addClass('btn btn-primary')
+                    .addClass('bg-theme border-0')
+                    .addClass('flex-shrink-0')
                     .on('click', () => remove(key))
-
             )
     )
-    // final
+    return key
+}
+
+function add() {
+    let key = newNode()
+
+    todos[key] = ""
+    save()
+
     counter += 1
+    store.setItem(LS_KEY_COUNTER, counter)
+}
+
+function clear(){
+    window.localStorage.clear()
+    counter = 0
+    todos = {}
+
+    $('#todo-list').empty()
 }
 
 // Init
-let addBtn = document.querySelector('button.add')
-addBtn.addEventListener('click', add)
-
-add()
-add()
+$('button.add').on('click', add)
+$('button.clear').on('click', clear)
+for(let key in todos) {
+    let val = todos[key]
+    newNode(key, val)
+}
